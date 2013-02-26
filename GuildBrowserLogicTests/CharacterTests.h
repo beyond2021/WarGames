19eb7688a0ececf3d6c66e49a7765061dac94d23
Beyond2021 <keevin.mitchell@gmail.com>
2013-02-26 03:34:10 -0500

    Character tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by KEEVIN MITCHELL on 2/26/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
