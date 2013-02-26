9c126c96dae5ec037e19bd29e4c13214be70ff42
Beyond2021 <keevin.mitchell@gmail.com>
2013-02-26 02:26:11 -0500

    Unit Test 1

---
+++ GuildBrowserTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by KEEVIN MITCHELL on 2/25/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
